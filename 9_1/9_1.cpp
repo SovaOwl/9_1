4aa8aa5485bcf5d615bf4f89ed76613cc2362671
SovaOwl <sova27755@gmail.com>
2022-08-16 17:21:56 +0300

    First commit

---
+++ 9_1/9_1.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Fraction
+{
+private:
+	int numerator_;
+	int denominator_;
+
+public:
+	Fraction(int numerator, int denominator)
+	{
+		numerator_ = numerator;
+		denominator_ = denominator;
+	}
+	double Abs()
+	{
+		return numerator_ / denominator_;
+	} 
+
+	bool operator==(Fraction other)
+	{
+		return Abs() == other.Abs();
+	}
+	bool operator!=(Fraction other)
+	{
+		return !(*this == other);
+	}
+	bool operator>(Fraction other)
+	{
+		return Abs() > other.Abs();
+	}
+	bool operator<(Fraction other)
+	{
+		return other > *this;
+	}
+	bool operator>=(Fraction other)
+	{
+		return !(*this < other);
+	}
+	bool operator<=(Fraction other)
+	{
+		return !(*this > other);
+	}
+
+};
+
+int main()
+{
+	system("chcp 1251");
+
+	Fraction f1(4, 3);
+	Fraction f2(6, 11);
+
+	cout << "f1" << ((f1 == f2) ? " == " : " not == ") << "f2" << endl;
+	cout << "f1" << ((f1 != f2) ? " != " : " not != ") << "f2" << endl;
+	cout << "f1" << ((f1 < f2) ? " < " : " not < ") << "f2" << endl;
+	cout << "f1" << ((f1 > f2) ? " > " : " not > ") << "f2" << endl;
+	cout << "f1" << ((f1 <= f2) ? " <= " : " not <= ") << "f2" << endl;
+	cout << "f1" << ((f1 >= f2) ? " >= " : " not >= ") << "f2" << endl;
+
+	return 0;
+}
